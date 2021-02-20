f36563413462ada750d587cae4f0660d9ffb63e4
tomo-0131 <woovoodoor@yahoo.co.jp>
2021-02-20 21:33:52 +0900

    修正

--- components/Top.vue
+++ components/Top.vue
@@ -1,17 +1,13 @@
 <template>
   <v-row justify="center" align="center">
-    <v-col cols="12" lg="12" xs="12">
       <v-row>
         <br>
         <v-img
           data-aos="fade-up"
           class="img1"
-          width="1500"
-          height="250"
-          v-bind:src="require('@/assets/images/one.png')"
+          v-bind:src="require('@/assets/images/top.png')"
         ></v-img>
       </v-row>
-    </v-col>
     <div>
       <h2 data-aos='zoom-in' class="content-title">Let's Health Check !!</h2>
       <p data-aos='fade-up' data-aos-duration="1000" class="text-center">生活習慣病リスクを今すぐに調べましょう！</p>
@@ -21,8 +17,8 @@
     <div>
       <div class="about">
         <div class="me">
-          <h2 data-aos='zoom-in' data-aos-duration="1500"><font-awesome-icon icon="pen" class="icon" />脂質異常症チェック</h2>
-          <p class="text">脂質異常症チェック</p>
+          <h2 data-aos='zoom-in' data-aos-duration="1500" class="text-top">脂質異常症チェック</h2>
+          <p class="text-top">脂質異常症チェック</p>
           <v-btn
             data-aos='fade-up' data-aos-duration="1000"
             class="white--text"
@@ -73,9 +69,14 @@
 @media screen and (max-width: 768px){
   .img1 {
     display: inline-block;
+    margin-left: 80px;
+    max-height: 200px;
+    max-width: 300px;
+    margin-top: 60px;
   }
 	.about {
 		flex-direction: column;
+    text-align: center;
 	}
 	.me {
     margin: 0 auto;
@@ -86,17 +87,25 @@
   }
   .content-title {
     margin-top: 30px;
+    text-align: center;
+  }
+  .text-top {
+    text-align: center;
   }
 }
 
 @media screen and (min-width: 1025px){
   .img1 {
-    margin-right: 190px;
+    margin-left: 390px;
+    max-height: 200px;
+    max-width: 400px;
+    margin-top: 60px;
   }
 
   .content-title {
     text-align: center;
     margin-top: 35px;
+
   }
 
   .about {
