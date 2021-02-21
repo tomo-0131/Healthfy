b2308de2a9c92f9a63a7960cd856d04550e94fdc
tomo-0131 <woovoodoor@yahoo.co.jp>
2021-02-21 10:25:52 +0900

    footer修正

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,10 +1,19 @@
 <template>
-  <v-footer>
-    <v-col class="text-center" cols="12">
-      <div class="footer" color="white">
-        {{ new Date().getFullYear() }} — Copyright Tomoyuki Kumagai All rights
-        reserved
-      </div>
-    </v-col>
-  </v-footer>
+  <div class="wrapper">
+    <v-footer>
+      <v-col class="text-center" cols="12">
+        <div class="footer" color="white">
+          {{ new Date().getFullYear() }} — Copyright Tomoyuki Kumagai All rights
+          reserved
+        </div>
+      </v-col>
+    </v-footer>
+  </div>
 </template>
+
+<style>
+  .footer {
+    position: relative;/*②絶対位置*/
+    bottom: 0; /*下に固定*/
+  }
+</style>
